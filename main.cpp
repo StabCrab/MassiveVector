8c5c354593e322c8c68ac353604e0f27ddeeeaea
Trishin Nikita <trykrop@gmail.com>
2020-12-20 13:37:38 +0300

    Some changes

--- main.cpp
+++ main.cpp
@@ -7,19 +7,6 @@
 
 #define LENGTH_OF_NUMBER 100000000
 using namespace std;
-mutex addMutex;
-void countBitsWithMutex(const bool *number, long long int begin, long long int end, unsigned long long int& count)
-{
-    for (long long i = begin; i < end; ++i)
-    {
-        if (number[i])
-        {
-            addMutex.lock();
-            count++;
-            addMutex.unlock();
-        }
-    }
-}
 
 void countBitsWithoutMutex(const bool *number, long long int begin, long long int end, unsigned long long int& count)
 {
@@ -32,22 +19,6 @@ void countBitsWithoutMutex(const bool *number, long long int begin, long long in
     }
 }
 
-void threadCountWithMutex(bool* number, unsigned long long int& count, int numberOfThreads)
-{
-
-    vector<thread> th;
-
-    for (int i = 0; i < numberOfThreads; ++i)
-    {
-        th.push_back(thread(countBitsWithMutex, number, LENGTH_OF_NUMBER * i / numberOfThreads, LENGTH_OF_NUMBER * (i + 1) / numberOfThreads, ref(count)));
-    }
-
-    for (auto& t : th)
-    {
-        t.join();
-    }
-
-}
 
 void threadCountWithoutMutex(bool* number, unsigned long long int& count, const int numberOfThreads)
 {
@@ -103,19 +74,10 @@ int main()
     {
         unsigned long long int count = 0;
         auto start = chrono::steady_clock::now();
-        countBitsWithMutex(number, 0, LENGTH_OF_NUMBER, ref(count));
+        countBitsWithoutMutex(number, 0, LENGTH_OF_NUMBER, ref(count));
         auto end = chrono::steady_clock::now();
         std::chrono::duration<double> elapsed_seconds = end-start;
         std::cout <<"One thread count " <<count <<" elapsed time: " << elapsed_seconds.count() << "s" << endl;
-    }
-
-    {
-        unsigned long long int count = 0;
-        auto start = chrono::steady_clock::now();
-        threadCountWithMutex(number, ref(count), 2);
-        auto end = chrono::steady_clock::now();
-        std::chrono::duration<double> elapsed_seconds = end-start;
-        std::cout<< "Two threads count with mutex " << count << " elapsed time: " << elapsed_seconds.count() << "s" << endl;
     }
 
     {
@@ -130,29 +92,12 @@ int main()
     {
         unsigned long long int count = 0;
         auto start = chrono::steady_clock::now();
-        threadCountWithMutex(number, ref(count), 4);
-        auto end = chrono::steady_clock::now();
-        std::chrono::duration<double> elapsed_seconds = end-start;
-        std::cout<< "Four threads count with mutex " << count << " elapsed time: " << elapsed_seconds.count() << "s" << endl;
-    }
-
-    {
-        unsigned long long int count = 0;
-        auto start = chrono::steady_clock::now();
         threadCountWithoutMutex(number, ref(count), 4);
         auto end = chrono::steady_clock::now();
         std::chrono::duration<double> elapsed_seconds = end-start;
         std::cout<< "Four threads count without mutex " << count << " elapsed time: " << elapsed_seconds.count() << "s" << endl;
     }
 
-    {
-        unsigned long long int count = 0;
-        auto start = chrono::steady_clock::now();
-        threadCountWithMutex(number, ref(count), 10);
-        auto end = chrono::steady_clock::now();
-        std::chrono::duration<double> elapsed_seconds = end-start;
-        std::cout<< "Ten threads count with mutex " << count << " elapsed time: " << elapsed_seconds.count() << "s" << endl;
-    }
 
     {
         unsigned long long int count = 0;
